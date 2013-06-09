79eac5ea4cf75a8229e69af89671673081b7e44e
ahoward <ara.t.howard@gmail.com>
2013-06-09 08:58:59 -0600

    use cgi's escape

--- lib/tagz.rb
+++ lib/tagz.rb
@@ -7,7 +7,7 @@ unless defined? Tagz
     require 'cgi'
 
     def Tagz.version()
-      '9.6.2'
+      '9.7.0'
     end
 
     def Tagz.description
@@ -271,7 +271,7 @@ unless defined? Tagz
 
           def escape(string)
             return string if string.respond_to?(:html_safe)
-            Tagz.xchar.escape(string)
+            Tagz.escape(string)
           end
           alias_method 'h', 'escape'
 
@@ -360,69 +360,9 @@ unless defined? Tagz
         end
         Tagz.singleton_class{ define_method(:element){ Tagz.namespace(:Element) } }
 
-        module XChar
-        # http://intertwingly.net/stories/2004/04/14/i18n.html#CleaningWindows
-        #
-          CP1252 = {
-            128 => 8364, # euro sign
-            130 => 8218, # single low-9 quotation mark
-            131 =>  402, # latin small letter f with hook
-            132 => 8222, # double low-9 quotation mark
-            133 => 8230, # horizontal ellipsis
-            134 => 8224, # dagger
-            135 => 8225, # double dagger
-            136 =>  710, # modifier letter circumflex accent
-            137 => 8240, # per mille sign
-            138 =>  352, # latin capital letter s with caron
-            139 => 8249, # single left-pointing angle quotation mark
-            140 =>  338, # latin capital ligature oe
-            142 =>  381, # latin capital letter z with caron
-            145 => 8216, # left single quotation mark
-            146 => 8217, # right single quotation mark
-            147 => 8220, # left double quotation mark
-            148 => 8221, # right double quotation mark
-            149 => 8226, # bullet
-            150 => 8211, # en dash
-            151 => 8212, # em dash
-            152 =>  732, # small tilde
-            153 => 8482, # trade mark sign
-            154 =>  353, # latin small letter s with caron
-            155 => 8250, # single right-pointing angle quotation mark
-            156 =>  339, # latin small ligature oe
-            158 =>  382, # latin small letter z with caron
-            159 =>  376} # latin capital letter y with diaeresis
-
-        # http://www.w3.org/TR/REC-xml/#dt-chardata
-        #
-          PREDEFINED = {
-            38 => '&amp;', # ampersand
-            60 => '&lt;',  # left angle bracket
-            62 => '&gt;'}  # right angle bracket
-
-        # http://www.w3.org/TR/REC-xml/#charsets
-        #
-          VALID = [[0x9, 0xA, 0xD], (0x20..0xD7FF), (0xE000..0xFFFD), (0x10000..0x10FFFF)]
-
-          def XChar.escape(string)
-            return string if string.respond_to?(:html_safe)
-            string.unpack('U*').map{|n| xchr(n)}.join # ASCII, UTF-8
-          rescue
-            string.unpack('C*').map{|n| xchr(n)}.join # ISO-8859-1, WIN-1252
-          end
-
-          def XChar.xchr(n)
-            (@xchr ||= {})[n] ||= ((
-              n = XChar::CP1252[n] || n
-              n = 42 unless XChar::VALID.find{|range| range.include? n}
-              XChar::PREDEFINED[n] or (n<128 ? n.chr : "&##{n};")
-            ))
-          end
-        end
-        Tagz.singleton_class{ define_method(:xchar){ Tagz.namespace(:XChar) } }
-
         NoEscapeContentProc = lambda{|*contents| contents.join}
         Tagz.singleton_class{ define_method(:no_escape_content_proc){ Tagz.namespace(:NoEscapeContentProc) } }
-        EscapeContentProc = lambda{|*contents| Tagz.xchar.escape(contents.join)}
+        EscapeContentProc = lambda{|*contents| Tagz.escapeHTML(contents.join)}
         Tagz.singleton_class{ define_method(:escape_content_proc){ Tagz.namespace(:EscapeContentProc) } }
 
         NoEscapeKeyProc = lambda{|*values| values.join}
@@ -446,10 +386,10 @@ unless defined? Tagz
   # escape utils
   #
     def Tagz.escapeHTML(*strings)
-      Tagz.xchar.escape(strings.join)
+      CGI.escapeHTML(strings.join)
     end
     def Tagz.escape(*strings)
-      Tagz.xchar.escape(strings.join)
+      CGI.escapeHTML(strings.join)
     end
     def Tagz.escapeAttribute(*strings)
       CGI.escapeHTML(strings.join)
